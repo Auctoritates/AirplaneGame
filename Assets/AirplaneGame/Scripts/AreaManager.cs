c7a4c778105ebffb4c1728f53012717796cb6d50
Hanz10x <Hanz10x@outlook.jp>
2017-12-08 14:40:36 +0900

    エリアマネージャーを拡充した+ゴールマネージャーのキャンバスの解像度対応を行った

--- Assets/AirplaneGame/Scripts/AreaManager.cs
+++ Assets/AirplaneGame/Scripts/AreaManager.cs
@@ -1,23 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AreaManager : MonoBehaviour {
 
+	//場外判定用の変数
 	[SerializeField] private GameObject _Airplane;
-	private float _RangeXmin;
-	private float _RangeXmax;
-	private float _RangeYmin;
-	private float _RangeYmax;
-	private float _RangeZmin;
-	private float _RangeZmax;
-	private float _AlertX;
-	private float _AlertY;
-	private float _AlertZ;
-
+	private Vector3 _PositionNow;
+	private Vector3 _AbsolutePosNow;
+	private Vector3 _DefaultPos;
+	private Vector3 _Range;
+	private Vector3 _Alert;
+	private float _MagnificationAlert;
 	private int _RangeState;//0=エリア内, 1=アラート圏内, 2=エリア逸脱
 
-	
+	//エフェクト用の変数
+	//[SerializeField] private Image _Alert1;
+	private Color _WarningColor;
+	private Color _Clear;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -31,37 +33,59 @@ public class AreaManager : MonoBehaviour {
 
 		if (_RangeState == 1)
 		{
-			Debug.Log("エリア逸脱");
+			Alert();
 		}
 		else if (_RangeState == 2)
 		{
-			Debug.Log("エリア逸脱警告範囲内");	
-		}
-		
+			AreaOver();
+		}			
+
 	}
 	
 	void SetDefault() //初期値設定
 	{
-		_RangeXmin = -50000f;
-		_RangeXmax = _RangeXmin * -1;
-		_RangeYmin = -50000f;
-		_RangeYmax = _RangeYmin * -1;
-		_RangeZmin = -50000f;
-		_RangeZmax = _RangeZmin * -1;
-		
-		_AlertX = _RangeXmax * 0.8f;
-		_AlertY = _RangeYmax * 0.8f;
-		_AlertZ = _RangeZmax * 0.8f;
-		
+		_DefaultPos = _Airplane.transform.position;
+
+		_Range = new Vector3(20000f, 20000f, 20000f);
+		_MagnificationAlert = 0.8f;
+		_Alert = new Vector3(_Range.x * _MagnificationAlert, _Range.y * _MagnificationAlert, _Range.z * _MagnificationAlert);
+				
+		_Range = new Vector3(Math.Abs(_Range.x), Math.Abs(_Range.y), Math.Abs(_Range.z)); 
+		_Alert = new Vector3(Math.Abs(_Alert.x), Math.Abs(_Alert.y), Math.Abs(_Alert.z));
 		
 		_RangeState = 0;
+		
+		_Clear = Color.clear;
+		_WarningColor = new Vector4(1f, 0.4f, 0.4f);
 	}
 
-	void OutDetect()
+	void OutDetect() //場外逸脱を検出
 	{
-		//機体の座標取得
-		//座標の位置判定
-		
+		_PositionNow = _Airplane.transform.position;
+		_AbsolutePosNow.x = Math.Abs(_PositionNow.x);
+		_AbsolutePosNow.y = Math.Abs(_PositionNow.y);
+		_AbsolutePosNow.z = Math.Abs(_PositionNow.z);
+
 		_RangeState = 0;
+		if (_AbsolutePosNow.x > _Range.x || _AbsolutePosNow.y > _Range.y || _AbsolutePosNow.z > _Range.z)
+		{
+			_RangeState = 2;
+		}
+		else if (_AbsolutePosNow.x > _Alert.x || _AbsolutePosNow.y > _Alert.y || _AbsolutePosNow.z > _Alert.z)
+		{
+			_RangeState = 1;			
+		}		
+	
+	}
+	
+	void AreaOver ()//逸脱時の処理
+	{
+		Debug.Log("逸脱");
+		_Airplane.transform.position = _DefaultPos;
+	}
+
+	void Alert ()
+	{
+		Debug.Log("警告");		
 	}
 }
