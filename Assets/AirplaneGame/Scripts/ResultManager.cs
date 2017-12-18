9146abba57161c4568a4a2abf475cbbc07dbed2f
Hanz10x <Hanz10x@outlook.jp>
2017-12-18 14:42:47 +0900

    リザルト画面の処理を追記、エラーは出ている

--- Assets/AirplaneGame/Scripts/ResultManager.cs
+++ Assets/AirplaneGame/Scripts/ResultManager.cs
@@ -7,9 +7,20 @@ using UnityEngine.UI;
 public class ResultManager : MonoBehaviour 
 {
 	[SerializeField] private GameObject _ResultUI;
-	public string _ActionCode;
+	[SerializeField] private Image _ResultBackImage1;
+	[SerializeField] private Image _ResultBackImage2;
+	[SerializeField] private Text _ClearStringText;
+	[SerializeField] private Text _GetSStringText;
+	[SerializeField] private Text _GetSNumberText;
+	[SerializeField] private Text _TotalSStringText;
+	[SerializeField] private Text _TotalSNumberText;
+ 	public string _ActionCode;
 	public bool _IsResultOK;
 
+	private Image _TmpImage;
+	private Text _TmpText;
+	private Color _TmpColor;
+	private float _Alpha;
 	// Use this for initialization
 	void Start () 
 	{
@@ -30,6 +41,7 @@ public class ResultManager : MonoBehaviour
 			SetResultOff();
 		}
 	}
+
 	void SetDefault()
 	{
 		_ActionCode = "";
@@ -38,10 +50,35 @@ public class ResultManager : MonoBehaviour
 	void SetResultOn()
 	{
 		_ResultUI.SetActive(true);
-	}
+		_Alpha = 255f;
+//		SetColor();
+		}
 	void SetResultOff()
 	{
+		_Alpha = 0f;
+//		SetColor();		
 		_ResultUI.SetActive(false);				
+	}
+	void SetColor()
+	{
+		ImageColor(_ResultBackImage1);
+		ImageColor(_ResultBackImage2);
+		TextColor(_GetSNumberText);
+		TextColor(_GetSStringText);		
+		TextColor(_TotalSStringText);
+		TextColor(_TotalSNumberText);
+	}
+	void ImageColor(Image _TmpImage)
+	{
+		_TmpColor = _TmpImage.color;
+		_TmpColor.a = _Alpha;
+		_TmpImage.color = _TmpColor;		
+	}
+	void TextColor(Text _TmpText)
+	{
+		_TmpColor = _TmpText.GetComponent<Text>().color;
+		_TmpColor.a = _Alpha;
+		_TmpText.GetComponent<Text>().color = _TmpColor;
 	}
 	void Action()
 	{
