c49dc32c9175d3988a41a4c9ea30efe50b82d8ca
juncocoa0731 <juncocoa0731@MacBook-Pro-13.local>
2017-12-22 15:12:07 +0900

    高さの表示とマップの仮実装

--- Assets/AirplaneGame/Scripts/HUDText.cs
+++ Assets/AirplaneGame/Scripts/HUDText.cs
@@ -2,21 +2,15 @@
 using UnityEngine.UI;
 
 public class HUDText : MonoBehaviour {
-	[SerializeField] private Text throtolText;
-	[SerializeField] private Text scoreText;
+    [SerializeField] private GameObject airplane;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text heightText;
+    
+    public void UpdateScoreText(int score) {
+        scoreText.text = "Score:" + score + "P";
+    }
 
-	private void Start ()
-	{
-		throtolText = GetComponent<Text> ();
-	}
-
-	public void UpdateScoreText(int score)
-	{
-		scoreText.text = "Score:" + score + "P";
-	}
-
-	public void UpdateThrotolText()
-	{
-		throtolText.text = "スロットル:";
-	}
+    public void Update() {
+        heightText.text = "Height:" + (airplane.GetComponent<Transform>().position.y / 100 + 5000) + "km";
+    }
 }
---
+++ Assets/AirplaneGame/Scripts/RadarManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarManager : MonoBehaviour {
+	[SerializeField] private GameObject airplane;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 test = airplane.GetComponent<Transform>().position;
+		GetComponent<Transform>().position += airplane.GetComponent<Transform>().position;
+
+	}
+}
